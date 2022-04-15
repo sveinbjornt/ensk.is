f063130d7664cf859b498cf469801b5a2a4cb5e2
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2022-04-15 15:08:53 +0000

    Vocab additions + added missing.py

--- missing.py
+++ missing.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
 
-from util import read_pages
+from db import EnskDatabase
 from pprint import pprint
+from util import read_wordlist
+
+# Initialize database singleton
+e = EnskDatabase()
+
+# Read all dictionary entries into memory
+res = e.read_all_entries()
+
+dict_words = [e["word"] for e in res]
 
 
-# r = read_pages(fn="add.txt")
-# for i in r:
-#     parse_line
-# pprint(r)
+top10k = read_wordlist("data/wordlists/google-10000-english-usa.txt")
 
+for t in top10k:
+    if t not in dict_words:
+        print(t)
