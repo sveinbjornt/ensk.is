5a14cde0b9f2371cb2d9b1762846cbb429212c14
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2023-06-08 21:37:37 +0000

    Fixes

---
+++ sort_additions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from dict import read_raw_pages, parse_line
+from db import EnskDatabase
+
+from pprint import pprint
+
+e = EnskDatabase()
+
+
+raw = read_raw_pages(fn="_add.txt")["_add"]
+
+radd = []
+
+raw = list(raw)
+
+raw = sorted(raw, key=lambda x: x[0])
+
+# pprint(raw)
+
+for line in raw:
+    print(line)
