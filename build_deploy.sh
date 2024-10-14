80a13977e0988af51cd8a25fb77736879a1cb3ad
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2024-10-14 21:45:29 +0000

    Deduplication, updated deployment script, rm dir not needed in version control

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,6 +1,16 @@
 #!bin/bash
+#
+# Build dictionary and audio files, then deploy to ensk.is
+#
 
 source venv/bin/activate
 python gen.py
 python audio.py
-bash deploy.sh
+
+/usr/bin/rsync \
+--exclude ".git" \
+--exclude "venv" \
+--exclude "p312" \
+--delete \
+-av "." \
+root@ensk.is:/www/ensk.is/html/
