a30f7831d595aa6da56d885dcc999fd1c70c9d7a
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2025-09-19 00:29:29 +0000

    Added missing_synonyms.py script

---
+++ missing_synonyms.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Finds all synonyms for words in the dictionary and prints out those
+that are not themselves in the dictionary.
+"""
+
+import nltk
+from dict import read_all_words, synonyms_for_word
+
+
+def main():
+    """
+    Get all synonyms for every word in the dictionary,
+    and then check whether the dictionary contains all the synonyms.
+    If not, print those synonyms to stdout.
+    """
+    nltk.download("wordnet")
+
+    from util import read_wordlist
+
+    ignore_words = set(read_wordlist("missing.txt"))
+
+    all_words = read_all_words()
+    word_set = set(all_words)
+
+    missing_synonyms = set()
+
+    for word in all_words:
+        synonyms = synonyms_for_word(word)
+        for synonym in synonyms:
+            if " " not in synonym and synonym not in word_set and synonym not in ignore_words:
+                missing_synonyms.add(synonym)
+
+    for missing in sorted(list(missing_synonyms)):
+        print(missing)
+
+
+if __name__ == "__main__":
+    main()
