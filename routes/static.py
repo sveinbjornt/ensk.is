f517b16250d73229680548cf8d9638a07bfa9c8e
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2026-09-06 12:08:43 +0000

    Don't cache static root routes

--- routes/static.py
+++ routes/static.py
@@ -1,39 +1,34 @@
 """
 Ensk.is
-Static resource routes
+Static file routes in root
 """
 
 from fastapi import APIRouter, Request
 from fastapi.responses import FileResponse, RedirectResponse
 
-from util import cache_response
 
 router = APIRouter()
 
 
 @router.get("/apple-touch-icon.png", include_in_schema=False)
 @router.head("/apple-touch-icon.png", include_in_schema=False)
-@cache_response
 async def apple_touch_icon_redirect(request: Request) -> RedirectResponse:
     return RedirectResponse(url="static/img/apple-touch-icon.png", status_code=301)
 
 
 @router.get("/apple-touch-icon-precomposed.png", include_in_schema=False)
 @router.head("/apple-touch-icon-precomposed.png", include_in_schema=False)
-@cache_response
 async def apple_touch_icon(request: Request) -> RedirectResponse:
     return RedirectResponse(url="static/img/apple-touch-icon.png", status_code=301)
 
 
 @router.get("/favicon.ico", include_in_schema=False)
 @router.head("/favicon.ico", include_in_schema=False)
-@cache_response
 async def favicon(request: Request) -> FileResponse:
     return FileResponse("static/img/favicon.ico", media_type="image/x-icon")
 
 
 @router.get("/robots.txt", include_in_schema=False)
 @router.head("/robots.txt", include_in_schema=False)
-@cache_response
 async def robots(request: Request) -> FileResponse:
     return FileResponse("static/files/robots.txt", media_type="text/plain")
